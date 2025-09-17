13baf202ffc93f927f3998a933e45be086aa2bc6
tbalson <tbalson@iu.edu>
2025-09-16 21:08:08 -0400

    updated comma after new model instance

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if "AZ_OPENAI_API_KEY" not in os.environ:
     ]
 else:
     MODELS = ["azure-openai/gpt-4o",
-              "azure-openai/gpt-4o-test"]
+              "azure-openai/gpt-4o-test",]
 
 
 st.set_page_config(
